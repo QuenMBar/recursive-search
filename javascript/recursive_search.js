ff225c17244e99a5f35890e63ccdef5d12810c5f
QuenMBar <Qbeast360@gmail.com>
2021-03-15 13:48:52 -0700

    Completed assignment

--- javascript/recursive_search.js
+++ javascript/recursive_search.js
@@ -1,16 +1,29 @@
+/**
+ *
+ * @param {array} arr
+ * @param {number} target
+ * @returns
+ */
 function recursiveSearch(arr, target) {
-  // type your code here
+    if (arr.length === 0) {
+        return false;
+    } else if (arr[arr.length - 1] === target) {
+        return true;
+    } else {
+        arr.pop();
+        return recursiveSearch(arr, target);
+    }
 }
 
 if (require.main === module) {
-  // add your own tests in here
-  console.log("Expecting: true");
-  console.log("=>", recursiveSearch([1, 2, 3], 2));
+    // add your own tests in here
+    console.log("Expecting: true");
+    console.log("=>", recursiveSearch([1, 2, 3], 2));
 
-  console.log("");
+    console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", recursiveSearch([3, 2, 1], 4));
+    console.log("Expecting: false");
+    console.log("=>", recursiveSearch([3, 2, 1], 4));
 }
 
 module.exports = recursiveSearch;
